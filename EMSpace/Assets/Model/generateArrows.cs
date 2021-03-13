2384a264e1d9bbe091345b12bfc4fd5df9b52ca0
BramCole <46005397+BramCole@users.noreply.github.com>
2021-03-13 14:46:51 -0500

    C# naming convention fixed
    funny the whole thing breaks if the filename is not the same as the classname

--- EMSpace/Assets/Model/generateArrows.cs
+++ EMSpace/Assets/Model/generateArrows.cs
@@ -6,7 +6,7 @@ using System;
 //evenly spaced square instead of sphere
 
 
-public class AnalyticCharge : MonoBehaviour
+public class GenerateArrows : MonoBehaviour
 {
 
 
@@ -92,7 +92,7 @@ public class AnalyticCharge : MonoBehaviour
                     //------------------------------needs to be refactored so we can retain code cohesion--------  //ie this is where we call whatever field funtion we are using(eg get capacitor)
                     //currently just using point charge
 
-                    eField = Capacitor(fieldPointPos, originPoint, pointSourceCharge);
+                    eField = ElectrostaticsFunctions.Capacitor(fieldPointPos, originPoint, pointSourceCharge);
 
                     //-------------------------------------------------------------------------------
                     Vector3 eHat = Vector3.Normalize(eField);
@@ -162,115 +162,4 @@ public class AnalyticCharge : MonoBehaviour
         
     }
 
-    Vector3 LineCharge(Vector3 fieldPointPos, Vector3 pointSourcePos, float charge, float length = 0.9f, int N = 13)
-    {
-        // E field from line of charge
-
-        // fieldPointpos == measurment point
-        // pointSourcepos == start of line of charge
-        // charge == total charge of line
-        // length == length of line
-        // direction == direction of line from pointsourcepos
-        // N == Number of points on line
-        
-        Vector3 totalEfield = new Vector3(0, 0, 0);
-        Vector3 direction = new Vector3(1, 0, 0);
-
-        direction = direction.normalized;
-        // divide charge and length by number of points
-        float dq = charge / (float)N;
-        float dL = length / (float)(N - 1);
-        // Calculate vector to next point
-        Vector3 L_step = dL * direction;
-
-        // step through line points and treat each as a point charge
-        for (int i = 0; i < N; i++)
-        {
-            Vector3 linePoint = pointSourcePos + (i * L_step);
-
-            Vector3 eField = SinglePointCharge(fieldPointPos, linePoint, dq);
-            // sum up efield from each point
-            totalEfield = totalEfield + eField;
-
-        }
-        return totalEfield;
-    }
-
-
-    Vector3 SinglePointCharge(Vector3 fieldPointPos, Vector3 pointSourcePos, float pointSourceCharge)
-    {
-        float coloumbConstant = 9 * 10 ^ 9;
-        Vector3 R = fieldPointPos - pointSourcePos;
-        Vector3 eField = (coloumbConstant * pointSourceCharge) / (Mathf.Pow(Vector3.Magnitude(R), 2)) * Vector3.Normalize(R);
-
-        return eField;
-    }
-
-
-    Vector3 plateCharge(Vector3 fieldPointPos, Vector3 plateSourcePos, float charge, float length = 0.6f, int N = 100)
-    {
-        // E field from square plate of charge in xy plane
-        // Probably wouldn't be too difficult to adjust for arbitrary orientation
-        // Function models a plate as an NxN array of point charges. Calculates as N line charges of length N with equal spacing
-
-        // fieldPointpos == measurment point
-        // pointSourcepos == Centre of plate
-        // charge == total charge of plate
-        // length == length of one side of plate
-        // N == Square root of number of points on plate
-        Vector3 totalEfield = new Vector3(0, 0, 0);
-
-        // Calculate the total charge of each line
-        float dq_line = charge / (float)N;
-        // calculate the distance between points
-        float dL = length / (float)(N - 1);
-        // Lines are always pointing in positive X
-        Vector3 direction = new Vector3(1, 0, 0);
-        // find the coordinates of bottom left corner of plate
-        Vector3 bottomLeftPlate = plateSourcePos - new Vector3(length / 2, length / 2, 0);
-        // Step through each line charge
-        for (int i = 0; i < N; i++)
-        {
-            // Start of first line is bottomLeftPlate. Subsequent lines start dL away in positive y direction
-            Vector3 lineStart = bottomLeftPlate + i * dL * new Vector3(0, 1, 0);
-            // Calculate E field from each line charge and sum
-            Vector3 eField = LineCharge(fieldPointPos, lineStart, dq_line, length, N);
-            totalEfield = totalEfield + eField;
-
-
-        }
-
-        return totalEfield;
-
-    }
-
-    Vector3 plateCharge_analytical(Vector3 fieldPointPos, Vector3 plateSourcePos, float charge, float length = 0.6f, int N = 100)
-    {
-        float top = length * length;
-        Vector3 rad = fieldPointPos - plateSourcePos;
-        float r = rad.magnitude;
-        float a = length;
-        float b = length;
-
-        float sigma = charge / (length * length);
-
-        float bottom = 4 * r * (float)Math.Sqrt((a / 2) * (a / 2) + (b / 2) * (b / 2) + r * r);
-
-        float arc = sigma * (float)Math.Atan2(top, bottom);
-
-        return sigma * rad.normalized;
-    }
-
-
-    Vector3 Capacitor(Vector3 fieldPointPos, Vector3 plateSourcePos, float charge, float seperationZ = 0.30f)
-    {
-        plateSourcePos = plateSourcePos + new Vector3(0, 0, 0);
-        Vector3 secondPlate = plateSourcePos + new Vector3(0, 0, seperationZ);
-
-        Vector3 totalEfield = plateCharge_analytical(fieldPointPos, plateSourcePos, -charge);
-        totalEfield = totalEfield + plateCharge_analytical(fieldPointPos, secondPlate, -charge);
-
-        return totalEfield;
-
-    }
 }
